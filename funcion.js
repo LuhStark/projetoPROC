5e3cf09041a1539e75fd25c3d09db7c9e3dc496f
Luana Carneiro <luanavieira2011@live.com>
2023-08-28 16:04:37 -0300

    and

---
+++ funcion.js
@@ -0,0 +1,23 @@
+function concluir(fichacompleta) {
+    var formulario = document.getElementById(fichacompleta);
+    //PERCORRER O FORMULARIO
+    //VERIFICAR OS ELEMENTOS OBRIGATORIOS
+}
+function soma(ausente_h1()) {
+    var formulario = soma(resultado_1a)
+    var ausente_h1 = 0
+    var presente_raramente_h1 = 2
+    var presente_frequentimente_h1 = 4
+    var ausente_h2 = 0
+    var presente_raramente_h2 = 2
+    var presente_frequentimente_h2 = 4
+    var ausente_h3 = 0
+    var presente_raramente_h3 = 2
+    var presente_frequentimente_h3 = 4
+    var ausente_h4 = 0
+    var presente_raramente_h4 = 2
+    var presente_frequentimente_h4 = 4
+    var resultado_1a = soma(
+        if soma 
+    )
+}
